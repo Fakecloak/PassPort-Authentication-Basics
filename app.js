a72dcd1c44fa7bef26559f308f2abce0d8cc0d28
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-07-07 00:29:42 +0530

    add local strategy using passport

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const pool = new Pool({
   // add your configuration
+    user: 'postgres',
+    host: 'localhost',
+    database: 'users',
+    password: 'test123',
+    port: 5432,
 });
 
 const app = express();
@@ -19,7 +24,64 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+
 app.get("/", (req, res) => res.render("index"));
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch(err) {
+    return next(err);
+  }
+});
+
 
 app.listen(3000, (error) => {
   if (error) {
